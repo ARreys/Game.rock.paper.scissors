71245abb07641a46007192649e491e7e3fcad849
Arthur Alves <art4321@outlook.com.br>
2020-02-06 23:44:34 -0300

    Rock_Paper_Scissor_v1

---
+++ ppt/src/Rps/ppt.java
@@ -0,0 +1,15 @@
+package Rps;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author arthuralves
+ */
+public class ppt {
+    public static void main(String[]args){
+        JOptionPane.showMessageDialog(null, "How to play? Then, to play is quite simple, "
+                + "just choose one of the alternatives to choose pieces and click (Run forest ... run)"
+                + " and the result will be shown.");
+        new Janela();
+    }
+}
